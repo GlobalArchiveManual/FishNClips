319ac94bc232581b34efcdcfe5327fc2c0c3c0b9
BrookeGibbons <21301367@student.uwa.edu.au>
2020-12-07 08:52:52 +0800

    update acknowledgements

--- ui.R
+++ ui.R
@@ -39,10 +39,10 @@ tagList(
                 ),
                 box(width = 8, status = "primary", height = 800, title = "Acknowledgments",
                     "The Marine Biodiversity Hub is funded by the Australian Government's National Environmental Science Program.", br(), br(),
-                    "Ningaloo Marine Park video footage from the benchmark survey of deepwater fish in the Ningaloo Marine Park, Commonwealth waters. Funded by the Marine Biodiversity Hub, Parks Australia, the CSIRO and UWA.", br(),br(),
-                    "Geographe Bay Marine Park video footage from the benchmark survey funded by the National Environmental Research Programme, UWA and Curtin University.",br(),br(),
+                    "Ningaloo Marine Park video footage from the benchmark survey of deepwater fish in the Ningaloo Marine Park, Commonwealth waters. Funded by the Marine Biodiversity Hub, Parks Australia, the CSIRO and the University of Western Australia.", br(),br(),
+                    "Geographe Bay Marine Park video footage from the benchmark survey funded by the National Environmental Research Programme, the University of Western Australia and Curtin University.",br(),br(),
                     "South-west Corner video footage is from the benchmark survey funded by by Parks Australia, the University of Western Australia and the Australian Government's National Environmental Science Program's Marine Biodiversity Hub. ",br(),br(),
-                    "These surveys are contributing to a benchmark our understanging of deeper water fishes and habitats, helping Parks Australia manage Australia's marine environment.",br(),br(),
+                    "These surveys are contributing to our understanding of deeper water fishes and habitats, helping Parks Australia manage Australia's marine environment.",br(),br(),
                     "Icons made by", a("Smashicons", href = "www.flaticon.com/authors/smashicons"), "from", a("Flaticon", href = "www.flaticon.com"))
                 )
         )
